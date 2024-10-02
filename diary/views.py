fac0da30908cecde6a7e9db865a7dfa9e5268298
Aladser <Aladser@users.noreply.github.com>
2024-10-02 22:42:10 +0900

    добавлены тематические css стили

--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.urls import reverse
+import os
 
+from django.shortcuts import render
+from dotenv import load_dotenv
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {'header': os.getenv('APP_NAME')})
