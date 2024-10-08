e57825f004baff75ca40a31b507317a0ba7f3975
Aladser <Aladser@users.noreply.github.com>
2024-10-08 17:04:03 +0900

    добавлен аватар

--- authen/models.py
+++ authen/models.py
@@ -1,13 +1,15 @@
+import os.path
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from config.settings import NULLABLE
+from config.settings import NULLABLE, MEDIA_ROOT
 
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(verbose_name='почта', unique=True)
     phone = models.CharField(verbose_name='телефон', unique=True, max_length=20, **NULLABLE)
-    avatar = models.ImageField(verbose_name='аватар', upload_to='img/user', **NULLABLE)
+    avatar = models.ImageField(verbose_name='аватар', upload_to='images/', **NULLABLE)
     token = models.CharField(verbose_name="Токен", **NULLABLE, max_length=100)
 
     USERNAME_FIELD = "email"
@@ -17,6 +19,11 @@ class User(AbstractUser):
         verbose_name = 'пользователь'
         verbose_name_plural = 'пользователи'
         ordering = ('first_name', 'last_name', 'email')
+
+    def clean(self):
+        avatar = os.path.join(MEDIA_ROOT, self.avatar.name)
+        if not os.path.exists(avatar):
+            self.avatar.name = 'images/PHOTO_NOT_FOUND.png'
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}" if self.first_name and self.last_name else self.email
